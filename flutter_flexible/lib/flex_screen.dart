b72a7ff2df27df176ac98948a8a369ac69ccd3a9
commanderk33n <torsten.m85@gmail.com>
2022-01-30 09:04:47 +0100

    expanded, flexible widgets and null safety

--- flutter_flexible/lib/flex_screen.dart
+++ flutter_flexible/lib/flex_screen.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flexible/labeled_container.dart';
 
 class FlexScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flexible and Expanded'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ..._header(context, 'Expanded'),
-          _buildExpanded(context),
-          ..._header(context, 'Flexible'),
-          _buildFlexible(context),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Flexible and Expanded'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ..._header(context, 'Expanded'),
+            _buildExpanded(context),
+            ..._header(context, 'Flexible'),
+            _buildFlexible(context),
+            Expanded(
+              child: Container(),
+            ),
+            _buildFooter(context)
+          ],
+        ),
       ),
     );
   }
@@ -33,13 +40,77 @@ Iterable<Widget> _header(BuildContext context, String text) {
 }
 
 Widget _buildExpanded(BuildContext context) {
-  return Container();
+  return SizedBox(
+    height: 100,
+    child: Row(
+      children: [
+        LabeledContainer(
+          width: 100,
+          color: Colors.green,
+          text: '100',
+        ),
+        Expanded(
+          child: LabeledContainer(
+            color: Colors.purple,
+            text: 'The Remainder',
+            textColor: Colors.white,
+          ),
+        ),
+        LabeledContainer(
+          width: 40,
+          color: Colors.green,
+          text: '40',
+        ),
+      ],
+    ),
+  );
 }
 
 Widget _buildFlexible(BuildContext context) {
-  return Container();
+  return SizedBox(
+    height: 100,
+    child: Row(
+      children: [
+        Flexible(
+          flex: 1,
+          child: LabeledContainer(
+            color: Colors.orange,
+            text: '25%',
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: LabeledContainer(color: Colors.deepOrange, text: '25%'),
+        ),
+        Flexible(
+          flex: 2,
+          child: LabeledContainer(
+            color: Colors.blue,
+            text: '50%',
+          ),
+        ),
+      ],
+    ),
+  );
 }
 
 Widget _buildFooter(BuildContext context) {
-  return Container();
+  return Center(
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 15.0,
+          horizontal: 30,
+        ),
+        child: Text(
+          'Pinned to the Bottom',
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+      ),
+    ),
+  );
 }
--- flutter_flexible/lib/labeled_container.dart
+++ flutter_flexible/lib/labeled_container.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 class LabeledContainer extends StatelessWidget {
-  final double width;
+  final double? width;
   final double height;
-  final Color color;
+  final Color? color;
   final String text;
-  final Color textColor;
+  final Color? textColor;
 
   const LabeledContainer({
-    Key key,
+    Key? key,
     this.width,
     this.height = double.infinity,
     this.color,
-    @required this.text,
+    required this.text,
     this.textColor,
   }) : super(key: key);
 
